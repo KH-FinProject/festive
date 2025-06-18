fdd6a1895ceaafe6ae2c735e6d5cc406581d3a9e
SemK0205 <s413625@gmail.com>
2025-06-18 14:22:01 +0900

    헤더 풋터 배포

---
+++ festive-app/src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import mainLogo from "../assets/festiveLogo.png";
+import searchbtn from "../assets/searchbtn.png";
+import "./HeaderFooter.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src={mainLogo} alt="festive logo" />
+      </div>
+      <nav className="nav">
+        {[
+          "이달의 축제",
+          "축제달력",
+          "지역별 축제",
+          "와글와글",
+          "AI 여행코스 추천",
+          "고객센터",
+          "부스참가신청",
+        ].map((item) => (
+          <a key={item} href="#" className="nav-link hover-grow">
+            {item}
+          </a>
+        ))}
+      </nav>
+      <div className="header-right">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="검색어를 입력해 주세요."
+        />
+        <img src={searchbtn} className="search-btn" />
+        <div className="weather-placeholder">날씨 API 자리</div>
+        <a href="#" className="nav-link hover-grow">
+          Sign In
+        </a>
+        <a href="#" className="nav-link hover-grow">
+          Sign Up
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- festive-app/src/main.jsx
+++ festive-app/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <div>
+    <Header />
+    <Footer />
+  </div>
+);
