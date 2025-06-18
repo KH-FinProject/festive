4ef34207ee712328ab90621b7efff4000f637fb2
COQUTE <kimjiho246@naver.com>
2025-06-18 10:25:53 +0900

    festive 초기 설정 - festive-app(React) - festiveServer(SpringBoot)

---
+++ festiveServer/src/main/java/com/project/festive/festiveserver/FestiveServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.festive.festiveserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestiveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestiveServerApplication.class, args);
+	}
+
+}
---
+++ festiveServer/src/test/java/com/project/festive/festiveserver/FestiveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.festive.festiveserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestiveServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
