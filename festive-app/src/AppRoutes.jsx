1c12e487d9b3f7291b833d7e55bdf7de9dc4ea8e
dudals3530 <dudals3530@naver.com>
2025-06-23 17:30:47 +0900

    AI추천코스 수정중

--- festive-app/src/AppRoutes.jsx
+++ festive-app/src/AppRoutes.jsx
@@ -3,11 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import MainPage from "./scomponents/mainPage/mainPage.jsx";
 import Signin from "./jcomponents/Signin/Signin.jsx";
 import Signup from "./jcomponents/Signup/Signup.jsx";
-import WaglePage from "./ycomponents/WaglePage.jsx";
-import WritePage from "./ycomponents/WritePage.jsx";
+import WaglePage from "./ycomponents/wagle/WaglePage.jsx";
+import WritePage from "./ycomponents/wagle/WritePage.jsx";
 import Find from "./jcomponents/FindId/Find.jsx";
 import FestivalMainPage from "./scomponents/monthFestive/This-month-festive.jsx";
-import WagleDetail from "./ycomponents/WagleDetail.jsx";
+import WagleDetail from "./ycomponents/wagle/WagleDetail.jsx";
 import FestiveCalendar from "./kcomponents/festiveCalendar/FestiveCalendar.jsx";
 import MyPageEditProfile from "./kcomponents/myPage/MyPageEditProfile.jsx";
 import MyPageEditInfo from "./kcomponents/myPage/MyPageEditInfo.jsx";
@@ -31,7 +31,7 @@ const AppRoutes = () => {
       <Route path="/wagle/:id" element={<WagleDetail />} />
       <Route path="/this-month" element={<FestivalMainPage />} />
       <Route path="/ai-travel" element={<AITravelCourse />} />
-      {/* <Route path="/ai-travel/chat" element={<AIChatbot />} /> */}
+      <Route path="/ai-travel/chat" element={<AIChatbot />} />
       <Route path="/calendar" element={<FestiveCalendar />} />
       <Route path="/mypage/profile" element={<MyPageEditProfile />} />
       <Route path="/mypage/info" element={<MyPageEditInfo />} />
