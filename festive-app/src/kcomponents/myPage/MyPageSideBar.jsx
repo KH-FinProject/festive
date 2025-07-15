4b28aa7e8439b348d0ae7a0c36a1a226723c7138
dudals3530 <dudals3530@naver.com>
2025-07-15 15:04:10 +0900

    Fix: Remove urlUtils import to resolve build error

--- festive-app/src/kcomponents/myPage/MyPageSideBar.jsx
+++ festive-app/src/kcomponents/myPage/MyPageSideBar.jsx
@@ -1,7 +1,6 @@
 import { NavLink, useLocation } from "react-router-dom";
 import "./MyPageSideBar.css";
 import useAuthStore from "../../store/useAuthStore";
-import { getProfileImageSrc } from "../../utils/urlUtils";
 
 const MyPageSideBar = () => {
   const { member } = useAuthStore();
