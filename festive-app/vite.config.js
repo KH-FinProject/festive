77f0512f1700008de819e069433ab4821aca7402
jihyun-00 <rkdwl811@naver.com>
2025-07-21 15:58:42 +0900

    Update vite.config.js

--- festive-app/vite.config.js
+++ festive-app/vite.config.js
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: false,
+    https: true,
     host: true,
-    port: 5173,
+    port: 443,
     proxy: {
       // '/profile-images'로 시작하는 모든 요청을 프록시
       "/profile-images": {
